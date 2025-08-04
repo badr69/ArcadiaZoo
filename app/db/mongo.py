18b1fa7398a0cc678c133d5bdfb639b07a1b9145
badreddine <manoudb@yahoo.fr>
2025-08-04 12:39:30 +0200

    debug bdd

--- app/db/mongo.py
+++ app/db/mongo.py
@@ -2,14 +2,37 @@ from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 import os
 from dotenv import load_dotenv
+import traceback
 
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
 MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "zoo_db")
 
-client = MongoClient(MONGO_URI, server_api=ServerApi("1"))
-db = client[MONGO_DB_NAME]
+try:
+    client = MongoClient(MONGO_URI, server_api=ServerApi("1"))
+    db = client[MONGO_DB_NAME]
+    reviews_collection = db["reviews"]
+except Exception:
+    traceback.print_exc()
+    client = None
+    db = None
+    reviews_collection = None
 
-# Définition de la collection reviews
-reviews_collection = db["reviews"]
+
+
+# from pymongo import MongoClient
+# from pymongo.server_api import ServerApi
+# import os
+# from dotenv import load_dotenv
+#
+# load_dotenv()
+#
+# MONGO_URI = os.getenv("MONGO_URI")
+# MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "zoo_db")
+#
+# client = MongoClient(MONGO_URI, server_api=ServerApi("1"))
+# db = client[MONGO_DB_NAME]
+#
+# # Définition de la collection reviews
+# reviews_collection = db["reviews"]
