df6738f6b407612a16ce90637103152f67217a3d
badreddine <manoudb@yahoo.fr>
2025-09-09 13:11:23 +0200

    modif main

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def api_test():
 if __name__ == '__main__':
     # app.run(host="0.0.0.0", port=5000, debug=True)
 
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
 
 
 
