7af7ced926f905b8d006921e72b628b9d3606b4a
badreddine <manoudb@yahoo.fr>
2025-12-04 22:11:52 +0100

    modif fichier test

--- app/tests/test_create_habitats.py
+++ app/tests/test_create_habitats.py
@@ -1,10 +1,10 @@
 import unittest
-from app.models.habitat_model import Habitat
+from app.models.habitat_model import HabitatModel
 from app.db.psql import get_db_connection
 
 class TestHabitat(unittest.TestCase):
     def setUp(self):
-        self.name = "Jungle Amazonienne"
+        self.name = "Jungle Africaine"
         self.url_image = "uploads/habitats/jungle.jpg"
         self.description = "Habitat pour animaux tropicaux"
 
@@ -12,13 +12,13 @@ class TestHabitat(unittest.TestCase):
         self._delete_habitat_by_name(self.name)
 
     def test_create_habitat(self):
-        habitat = Habitat.create_habitat(self.name, self.url_image, self.description)
+        habitat = HabitatModel.create_habitat(self.name, self.url_image, self.description)
 
         self.assertIsNotNone(habitat)
         self.assertEqual(habitat.name, self.name)
         self.assertEqual(habitat.url_image, self.url_image)
         self.assertEqual(habitat.description, self.description)
-        self.assertIsNotNone(habitat.id)
+        self.assertIsNotNone(habitat.habitat_id)
         self.assertIsNotNone(habitat.created_at)
 
     def tearDown(self):
